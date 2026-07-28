311b2131c7c8e05ed46e58e7eb61085c5f42b586
nexdep <marco.depietri90@gmail.com>
2026-07-28 10:53:23 -0400

    data fetcher script improvement

--- home/dot_zsh/neutronics.zsh
+++ home/dot_zsh/neutronics.zsh
@@ -8,8 +8,3 @@ if [[ -f /opt/openfoam12/etc/bashrc ]]; then
   }
 fi
 
-# OpenMC data paths (endf/b viii.0 cross sections + endfb81 fast chain, as
-# downloaded by ~/.scripts/openmc_scripts/openmc_data_fetcher.sh)
-export OPENMC_CROSS_SECTIONS="$HOME/openmc_data/endfb-viii.0-hdf5/cross_sections.xml"
-export OPENMC_CHAIN_FILE="$HOME/openmc_data/chain_endfb81_fast/chain_endfb81_fast.xml"
-export ENDFB81_XS="$OPENMC_CROSS_SECTIONS"
